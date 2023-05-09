55f3d15c27a1fc4eec5600938dc18c9dd2c3820c
chlouzada <chlouzada@gmail.com>
2023-05-08 22:21:24 -0300

    m/pop

---
+++ 00016-medium-pop.ts
@@ -0,0 +1,15 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+  Expect<Equal<Pop<[]>, []>>,
+]
+
+
+// ============= Your Code Here =============
+type Pop<T extends any[], Initial = T,Last extends any = null> = T extends [...infer Target, infer Last] ? Target: T
+
+type Foo = Pop<[]>
+//   ^?
